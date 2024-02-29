1bffdb2f8a9801e8703e9bb0738917dbed192991
Tymoteusz Lango <58483383+tymek805@users.noreply.github.com>
2024-02-29 16:06:15 +0100

    Add movie related structures

---
+++ MoviesAPI/src/main/java/com/tl/moviesapi/controllers/MovieController.java
@@ -0,0 +1,29 @@
+package com.tl.moviesapi.controllers;
+
+import com.tl.moviesapi.models.Movie;
+import com.tl.moviesapi.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingularMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
---
+++ MoviesAPI/src/main/java/com/tl/moviesapi/services/MovieService.java
@@ -0,0 +1,23 @@
+package com.tl.moviesapi.services;
+
+import com.tl.moviesapi.models.Movie;
+import com.tl.moviesapi.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
